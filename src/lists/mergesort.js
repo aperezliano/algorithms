6702e9615306476af20204da25a643eea33ad955
lprez <lprez@ebay.com>
2021-04-17 15:29:36 +0200

    Mergesort, added base case with 2 elements

--- src/lists/mergesort.js
+++ src/lists/mergesort.js
@@ -2,6 +2,8 @@ module.exports = mergeSort;
 
 function mergeSort(list) {
   if (list === null || list.length <= 1) return list;
+  if (list.length === 2) return list[1] < list[0] ? [list[1], list[0]] : list;
+
   const half = Math.floor(list.length / 2);
   return merge(mergeSort(list.slice(0, half)), mergeSort(list.slice(half)));
 }
